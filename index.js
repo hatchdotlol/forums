e5f023d59183a70bebacd84446ce1cac39f140ec
qwertyytheartist <qwertyytheartist@gmail.com>
2025-01-07 02:25:53 +0000

    basic db stuff

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
+const sqlite = require("sqlite3");
 
 const app = express();
 
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
+
+const db = new sqlite.Database('./db.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+});
+
+db.run(`CREATE TABLE categories (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL
+  description TEXT NOT NULL
+)`, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+});
+
+db.run("INSERT INTO categories (name, description) VALUES ('Announcements', 'Announcements from the Hatch Team.')", (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+});
+
+db.run("INSERT INTO categories (name, description) VALUES ('Suggestions', 'Suggestions for feature additions to Hatch.')", (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+});
 
 app.use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
@@ -18,5 +47,5 @@ app.get("/api", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
+  console.log(`listening on http://localhost:${port}`);
 })
