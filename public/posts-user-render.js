a498d522a92af0eeaf59a210ddc11662482037df
breakfast <qwertyytheartist@gmail.com>
2025-01-09 15:14:05 -0800

    user role display

--- public/posts-user-render.js
+++ public/posts-user-render.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
         fetch(`https://api.hatch.lol/users/${element.id}`).then(res => res.json()).then(data => {
             element.querySelector(".post-user-name").innerText = data.displayName;
             element.querySelector(".post-pfp").src = data.profilePicture.startsWith("data:image") ? data.profilePicture : `https://api.hatch.lol/${data.profilePicture}`;
+            element.querySelector(".post-user-role").innerText = data.hatchTeam === "true" ? "Hatch Team" : "Hatchling";
         });
     });
 });
