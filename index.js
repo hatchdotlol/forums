3f3b4b3259585238a36154a2a591cdaa6983b673
breakfast <qwertyytheartist@gmail.com>
2025-02-27 15:12:26 -0800

    update + add new dependencies

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const sqlite = require("sqlite3");
 const bodyParser = require("body-parser");
+const requestIP = require("request-ip");
 
 const app = express();
 
